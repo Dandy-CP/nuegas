dc6a0d528a01e2a896f5144cd4ba82105ab53c46
Dandy Candra <dandycandrapratama@gmail.com>
2025-09-22 14:22:02 +0700

    Enhance ClassDetailPage to fetch and display class details; integrate ClassForum and ClassMember components with appropriate props

--- pages/dashboard/class/[classId].tsx
+++ pages/dashboard/class/[classId].tsx
@@ -1,9 +1,31 @@
+import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { Divider, Tab, Tabs } from '@mui/material';
-import { ClassForum } from '@/components/modules/class';
+import { ErrorView } from '@/components/modules';
+import { ClassForum, ClassMember } from '@/components/modules/class';
+import { GetClassDetail } from '@/service/api/class/class.query';
 
 function ClassDetailPage() {
 	const [tabValue, setTabValue] = useState(0);
+
+	const router = useRouter();
+	const classId = router.query.classId as string;
+
+	const { data, isError, refetch } = GetClassDetail(
+		{ class_id: classId },
+		{ queryKey: [classId], enabled: classId !== undefined }
+	);
+
+	const classDetail = data?.data;
+	const isClassOwner = data?.data.isOwner ?? false;
+
+	if (isError) {
+		return (
+			<div className='my-40'>
+				<ErrorView onRefetch={() => refetch()} />
+			</div>
+		);
+	}
 
 	return (
 		<div>
@@ -21,13 +43,21 @@ function ClassDetailPage() {
 			<Divider />
 
 			<div className='mt-10 px-15'>
-				{tabValue === 0 && <ClassForum />}
+				{tabValue === 0 && (
+					<ClassForum
+						classId={classId}
+						classDetail={classDetail}
+						isClassOwner={isClassOwner}
+					/>
+				)}
 
 				{tabValue === 1 && <h1>Assignment</h1>}
 
 				{tabValue === 2 && <h1>Quiz</h1>}
 
-				{tabValue === 3 && <h1>Member</h1>}
+				{tabValue === 3 && (
+					<ClassMember classId={classId} isClassOwner={isClassOwner} />
+				)}
 			</div>
 		</div>
 	);
