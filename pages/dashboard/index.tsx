7f1d314dbdc78e8f96176e1efc03084eb8e02b11
Dandy Candra <dandycandrapratama@gmail.com>
2025-12-10 14:39:58 +0700

    Removes MyClassList from Dashboard page
    The MyClassList component is no longer displayed on the dashboard, simplifying the page's layout.

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { JoinedClassList, MyClassList } from '@/components/modules/dashboard';
+import { JoinedClassList } from '@/components/modules/dashboard';
 
 function Index() {
 	return (
 		<div>
-			<MyClassList />
 			<JoinedClassList />
 		</div>
 	);
